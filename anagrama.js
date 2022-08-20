983cdb70cfbd9e504d106932735b9d18e9c46bcc
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-08-20 18:23:00 +0100

    Update anagrama.js

--- anagrama.js
+++ anagrama.js
@@ -32,5 +32,8 @@
 
 const buildCharObject = str => {
     const charObj = {}
-    for(let char of str.replace(/[^\w]g.toLowerCase()))
+    for (let char of str.replace(/[^\w]g).toLowerCase()){
+        charObj[char] = charObj[char] + 1 || 1
+    }
+    return charObj
 }
